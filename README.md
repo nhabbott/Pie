866c41e04b52b41bd7cc16fcf0d3c306f097d905
Noah <noah.abbott2019@mkchs.net>
2018-08-26 18:58:51 -0700

    Comments & README

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Pie
 A very rudimentary scripting language written in Golang.
 
+## Note
+At the moment the only functions pie posses is 'print', and only strings can be printed. Expressions are printed as text instead of being
+evaluated because I can't figure out how to implement this.
+
 ## Compilation Commands
 Normal: `go run pie.go -f=path/to/file.pie`
 Verbose: `go run pie.go -f=path/to/file.pie -v`
 
 ### In Progress
 - Lexer
+- Parser
+- Expressions
 
 ### To be Implemented
-- Parser
 - Features
   1. Functions
   2. Variables
-  3. Expressions (+, -, /, *)
--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Prints given string and removes anything added by tokenization
 func doPrint(toPrint string) {
 	if toPrint[0:6] == "string" {
 		toPrint = toPrint[8 : len(toPrint)-1]
@@ -15,6 +16,7 @@ func doPrint(toPrint string) {
 	fmt.Println(toPrint)
 }
 
+// Supposed to eval the numbers in a string; still haven't figured this out
 func eval(expr string) {
 
 }
@@ -23,6 +25,7 @@ func eval(expr string) {
 func Parse(tokens []string) {
 	i := 0
 
+	// Checks for print command and prints accordingly
 	for i < len(tokens) {
 		if tokens[i] == "print" {
 			if tokens[i+1][0:6] == "string" {
@@ -31,6 +34,7 @@ func Parse(tokens []string) {
 				doPrint(tokens[i+1])
 			} else if tokens[i+1][0:4] == "expr" {
 				doPrint(tokens[i+1])
+				eval(tokens[i+1])
 			}
 			i += 2
 		}
